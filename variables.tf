a78f5ea58af8293bbc9daaf1b50a26c276e60d42
Pavan Kumar <pavan_2004it@icloud.com>
2023-10-23 16:03:40 +0530

    added the dynamic rules

--- variables.tf
+++ variables.tf
@@ -14,34 +14,6 @@ variable "rule_group_name" {
   type = string
 }
 
-variable "identity_search_string" {
-  type = string
-}
-
-variable "portal_web_search_string" {
-  type = string
-}
-
-variable "custom_forms_search_string" {
-  type = string
-}
-
-variable "identity_rp_search_string" {
-  type = string
-}
-
-variable "forms_rp_search_string" {
-  type = string
-}
-
-variable "portal_rp_search_string" {
-  type = string
-}
-
-variable "search_strings" {
-  type = list(string)
-}
-
 variable "rp_rules" {
   type = map(string)
 }
