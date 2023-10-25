0a977f64fb563aee0a750942c1580c1c62fca592
Pavan Kumar <pavan_2004it@icloud.com>
2023-10-25 11:42:35 +0530

    added multi rule config

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "aws_wafv2_web_acl" "rp_web_acl" {
+resource "aws_wafv2_web_acl" "rp-web-acl" {
   name        = var.waf_name
-  description = "Web ACLc to attach to ALB"
-  scope       = "REGIONAL"
+  description = var.waf_description
+  scope       = var.waf_scope
 
   default_action {
     allow {}
@@ -12,250 +12,70 @@ resource "aws_wafv2_web_acl" "rp_web_acl" {
     }
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "WAF-ALB-Rule-Group"
+      metric_name                = var.waf_metric_name
       sampled_requests_enabled   = true
     }
-    name     = "WAF-ALB-Rule-Group"
+    name     = var.waf_rule_name
     priority = 0
     statement {
       rule_group_reference_statement {
-        arn = aws_wafv2_rule_group.alb_rule_group.arn
+        arn = aws_wafv2_rule_group.rp-rg.arn
       }
     }
   }
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = var.metric_name
+    metric_name                = var.waf_metric_name
     sampled_requests_enabled   = true
   }
-
 }
 
-#resource "aws_wafv2_rule_group" "alb_rule_group" {
-#  name        = var.rule_group_name
-#  description = "dev ALB Rule Group"
-#  capacity    = 1500
-#
-#  scope = "REGIONAL"
-#
-#  rule {
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-ALB-Portal-Web"
-#      sampled_requests_enabled   = true
-#    }
-#    name     = "Allow-ALB-Portal-Web"
-#    priority = 0
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.portal_web_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "Allow-Identity-Web"
-#    priority = 1
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-Identity-Web"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.identity_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "Allow-Custom-Forms"
-#    priority = 2
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-Custom-Forms"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.custom_forms_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "Allow-ALB-Identity-RP"
-#    priority = 3
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-ALB-Identity-RP"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.identity_rp_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "Allow-ALB-Forms-RP"
-#    priority = 4
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-ALB-Forms-RP"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.forms_rp_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "Allow-Portal-RP"
-#    priority = 5
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "Allow-Portal-RP"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      allow {}
-#    }
-#    statement {
-#      byte_match_statement {
-#        field_to_match {
-#          single_header {
-#            name = "host"
-#          }
-#        }
-#        positional_constraint = "EXACTLY"
-#        search_string         = var.portal_rp_search_string
-#        text_transformation {
-#          priority = 0
-#          type     = "NONE"
-#        }
-#      }
-#    }
-#  }
-#  rule {
-#    name     = "ALB-CDN-Rule"
-#    priority = 6
-#    visibility_config {
-#      cloudwatch_metrics_enabled = true
-#      metric_name                = "ALB-CDN-Rule"
-#      sampled_requests_enabled   = true
-#    }
-#    action {
-#      block {}
-#    }
-#    statement {
-#      not_statement {
-#        statement {
-#          byte_match_statement {
-#            field_to_match {
-#              single_header {
-#                name = "x-custom-header"
-#              }
-#            }
-#            positional_constraint = "EXACTLY"
-#            search_string         = "random-value-123456"
-#            text_transformation {
-#              priority = 0
-#              type     = "NONE"
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#
-#  visibility_config {
-#    cloudwatch_metrics_enabled = true
-#    metric_name                = "ALB-Rule-Group"
-#    sampled_requests_enabled   = true
-#  }
-#}
-
-resource "aws_wafv2_rule_group" "alb_rule_group" {
-  for_each = var.rp_rules
-  name = var.rule_group_name
-  description = "Prod Rule Group"
-  capacity    = 1500
-  scope = "REGIONAL"
-  rule = each.value
+resource "aws_wafv2_rule_group" "rp-rg" {
+  capacity = var.rule_group_capacity
+  name     = var.rule_group_name
+  scope    = var.rule_group_scope
+  dynamic "rule" {
+    for_each = var.waf_rules
+    content {
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value.visibility_config_metric_name
+        sampled_requests_enabled   = true
+      }
+      name = rule.value.name
+      priority = rule.value.priority
+      action {
+        dynamic "allow" {
+          for_each = rule.value.action == "allow" ? [1] : []
+          content {}
+        }
+      }
+      statement {
+        byte_match_statement {
+          field_to_match {
+            single_header {
+              name = rule.value.byte_match_header_name
+            }
+          }
+          positional_constraint = rule.value.byte_match_positional_constraint
+          search_string = rule.value.byte_match_search_string
+          text_transformation {
+            priority = rule.value.text_transformation_priority
+            type     = rule.value.text_transformation_type
+          }
+        }
+      }
+    }
+  }
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "ALB-Rule-Group"
+    metric_name                = var.rule_group_metric_name
     sampled_requests_enabled   = true
   }
 }
-
 
 resource "aws_wafv2_web_acl_association" "example_acl_association" {
   resource_arn = var.alb_arn
-  web_acl_arn  = aws_wafv2_web_acl.rp_web_acl.arn
+  web_acl_arn  = aws_wafv2_web_acl.rp-web-acl.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,50 @@
 variable "alb_arn" {
   type = string
 }
-
+variable "waf_description" {
+  type = string
+}
+variable "rule_group_metric_name" {
+  type = string
+}
+variable "rule_group_scope" {
+  type = string
+}
+variable "rule_group_capacity" {
+  type = number
+}
+variable "waf_scope" {
+  type = string
+}
+variable "waf_rule_name" {
+  type = string
+}
 variable "waf_name" {
   type = string
 }
-
-variable "metric_name" {
+variable "waf_metric_name" {
   type = string
 }
 
 variable "rule_group_name" {
   type = string
+  default = "rp-sample-rg"
 }
 
-variable "rp_rules" {
-  type = map(string)
+variable "waf_rules" {
+  description = "List of WAF rules"
+  type = list(object({
+    visibility_config_cloudwatch_metrics_enabled = bool
+    visibility_config_metric_name                = string
+    visibility_config_sampled_requests_enabled   = bool
+    name                                         = string
+    priority                                     = number
+    action                                       = string # "allow", "block", "count"
+    byte_match_search_string                     = string
+    byte_match_positional_constraint             = string
+    byte_match_header_name                       = string
+    text_transformation_priority                 = number
+    text_transformation_type                     = string
+  }))
 }
 
